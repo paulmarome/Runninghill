195413f65cae29f173995a09f31fbe2725034937
Paul Ntshabeleng <53702301+paulmarome@users.noreply.github.com>
2021-01-24 13:34:51 +0000

    Update CharacterSearch.cs

--- Character Search/Pre Solutions/CharacterSearch.cs
+++ Character Search/Pre Solutions/CharacterSearch.cs
@@ -1,4 +1,8 @@
-System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace Search
 {
     class CharacterSearch
